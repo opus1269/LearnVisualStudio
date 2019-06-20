6af756a8a34e1bbcd4f3ffb6db986281365c23e5
opus1269 <igreilly1269@gmail.com>
2019-06-19 21:29:50 -0500

    dont use using namespace

--- LearnVisualStudio/Point.h
+++ LearnVisualStudio/Point.h
@@ -2,15 +2,13 @@
 
 #include <iostream>
 
-using namespace std;
-
 class Point {
  public:
   Point() = default;
   Point(int x, int y) : m_x(x), m_y(y){};
   int x() { return m_x; }
   int y() { return m_y; }
-  friend ostream& operator<<(ostream& os, const Point& pt);
+  friend std::ostream& operator<<(std::ostream& os, const Point& pt);
 
  private:
   int m_x;
