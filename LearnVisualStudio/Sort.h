af98a209faa53fdb8c8e51fae73d107b6a83a030
opus1269 <igreilly1269@gmail.com>
2019-06-23 07:37:51 -0500

    Remove Sort.inl

--- LearnVisualStudio/Sort.h
+++ LearnVisualStudio/Sort.h
@@ -5,16 +5,45 @@ namespace mysort {
 using std::size_t;
 
 template <class T, size_t N>
-void mergeSort(T (&arr)[N], size_t beg, size_t end);
+void mergeSort(T (&arr)[N], size_t beg, size_t end) {
+  if (beg == end) {
+    return;
+  }
+
+  auto mid = (beg + end) / 2;
+  mergeSort(arr, beg, mid);
+  mergeSort(arr, mid + 1, end);
+
+  mysort::merge(arr, beg, end);
+}
 
 namespace {
 
 template <class T, size_t N>
-void merge(T (&arr)[N], size_t beg, size_t end);
+void merge(T (&arr)[N], size_t beg, size_t end) {
+  auto mid = (beg + end) / 2;
+  auto i = beg;
+  auto j = mid + 1;
+  auto len = end - beg + 1;
+  T *temp = new T[len];
 
+  for (size_t k = 0; k < len; k++) {
+    if ((j > end) || ((i <= mid) && (arr[i] < arr[j]))) {
+      temp[k] = arr[i];
+      i++;
+    } else {
+      temp[k] = arr[j];
+      j++;
+    }
+  }
+
+  for (size_t k = 0, i = beg; k < len; k++, i++) {
+    arr[i] = temp[k];
+  }
+
+  delete[] temp;
 }
 
-}  // namespace mysort
+}  // namespace
 
-// template definitions
-#include "Sort.inl"
+}  // namespace mysort
